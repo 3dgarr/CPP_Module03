d97758feb6f28096269f60dfd3f4ecf49241d5ca
3dgarr <edgarghazaryandoc@gmail.com>
2023-02-21 15:55:44 +0400

    ex00

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -4,15 +4,19 @@
 class ClapTrap
 {
 	public:
-        void	attack(const std::string& target);
-        void	takeDamage(unsigned int amount);
-        void	beRepaired(unsigned int amount);
+        void			attack(const std::string& target);
+        void			takeDamage(unsigned int amount);
+        void			beRepaired(unsigned int amount);
+		unsigned int	get_hitPoints() const;
+		unsigned int	get_energyPoints() const;
+		unsigned int	get_attackDamage() const;
+		std::string		get_name() const;
 
     public:
         ClapTrap();
         ClapTrap(const std::string&);
         ClapTrap(const ClapTrap&);
-        ClapTrap& operator=(const ClapTrap&)
+        ClapTrap& operator=(const ClapTrap&);
         ~ClapTrap();
 
     private:
@@ -23,11 +27,54 @@ class ClapTrap
 
 };
 
-ClapTrap::ClapTrap(/* args */)
+ClapTrap::ClapTrap()
+	:	name("gago"),
+		hitPoints(10),
+		energyPoints(10),
+		attackDamage(0)
 {
+	std::cout << "Default Constructor called" << std::endl;
+};
+
+ClapTrap::ClapTrap(const std::string& __name)
+	:	name(__name),
+		hitPoints(10),
+		energyPoints(10),
+		attackDamage(0)
+{
+	std::cout << "Parameterized Constructors  called" << std::endl;
+};
+
+        
+ClapTrap::ClapTrap(const ClapTrap& rhs)
+	:	name(rhs.get_name())
+
+{
+	std::cout << "Copy Constructors  called" << std::endl;
+
 
 }
 
+
 ClapTrap::~ClapTrap()
 {
+};
+
+unsigned int	ClapTrap::get_attackDamage() const
+{
+	return (attackDamage);
+};
+
+unsigned int	ClapTrap::get_energyPoints() const
+{
+	return (energyPoints);
+};
+unsigned int	ClapTrap::get_hitPoints() const
+{
+	return (hitPoints);
+}
+
+std::string		ClapTrap::get_name() const
+{
+	return (name);
 }
